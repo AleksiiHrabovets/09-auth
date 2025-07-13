fefd26df5a9282c99cea0e50f7bc94c785d79668
ALeksiiHrabovets <hrabovets78@gmail.com>
2025-07-13 15:06:04 +0300

    patch02

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,9 +2,13 @@ import axios from 'axios';
 import type { Note } from '../../types/note';
 
 export const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + '/api',
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   withCredentials: true,
 });
+
+const cleanParams = (params: Record<string, any>) => {
+  return Object.fromEntries(Object.entries(params).filter(([_, value]) => value !== undefined));
+};
 
 export type GetNotesRequest = {
   params: {
@@ -27,4 +31,16 @@ export type GetNotesResponse = {
 
 export type ServerBoolResponse = {
   success: boolean;
+};
+
+export const getNotes = async (request: GetNotesRequest): Promise<GetNotesResponse> => {
+  const { params, headers, withCredentials } = request;
+
+  const response = await nextServer.get<GetNotesResponse>('/notes', {
+    params: cleanParams(params),
+    headers,
+    withCredentials,
+  });
+
+  return response.data;
 };
